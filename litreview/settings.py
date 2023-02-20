44f3b5c2de7b45b4f55636c453302c25d19bb86c
rv <rv.monnier@free.fr>
2023-02-20 15:53:25 +0100

    add bootstrap 4 in requirements and in settings.py

--- litreview/settings.py
+++ litreview/settings.py
@@ -37,9 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_bootstrap4",
     "authentication",
     "flux",
-    "crispy_forms",
+    
 ]
 
 MIDDLEWARE = [
